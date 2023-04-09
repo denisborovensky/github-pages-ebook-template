15883b884c5487f43750b3c1fe617f646ced6f2c
Denis Borovensky <denisborovensky@gmail.com>
2023-04-09 19:53:23 +0300

    fix: some refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,27 +45,27 @@ var sectionHeight = function() {
     }
   }
   
-  $(window).resize(sectionHeight);
+$(window).resize(sectionHeight);
   
-  $(function() {
-    $("section h2, section h3").each(function(){
-      $("nav ul").append("<li class='tag-" + translit(this.nodeName.toLowerCase()) + "'><a href='#" + translit($(this).text().toLowerCase()) + "'>" + $(this).text() + "</a></li>");
-      $(this).attr("id",translit($(this).text().toLowerCase()));
-      $("nav ul li:first-child a").parent().addClass("active");
-    });
-  
-    $("nav ul li").on("click", "a", function(event) {
-      var position = $($(this).attr("href")).offset().top - 190;
-      $("html, body").animate({scrollTop: position}, 400);
-      $("nav ul li a").parent().removeClass("active");
-      $(this).parent().addClass("active");
-      event.preventDefault();
-    });
-  
-    sectionHeight();
-  
-    $('img').on('load', sectionHeight);
+$(function() {
+  $("section h2, section h3").each(function(){
+    $("nav ul").append("<li class='tag-" + translit(this.nodeName.toLowerCase()) + "'><a href='#" + translit($(this).text().toLowerCase()) + "'>" + $(this).text() + "</a></li>");
+    $(this).attr("id",translit($(this).text().toLowerCase()));
+    $("nav ul li:first-child a").parent().addClass("active");
   });
+
+  $("nav ul li").on("click", "a", function(event) {
+    var position = $($(this).attr("href")).offset().top - 190;
+    $("html, body").animate({scrollTop: position}, 400);
+    $("nav ul li a").parent().removeClass("active");
+    $(this).parent().addClass("active");
+    event.preventDefault();
+  });
+  
+  sectionHeight();
+  
+  $('img').on('load', sectionHeight);
+});
   
 function scrollToTop() {
   window.scrollTo(0, 0);
